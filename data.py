119d09776a3a1d717f98545f931fdd3164acc9eb
mnz0 <mansaaa@gmail.com>
2019-04-19 02:09:20 +0300

    data patterns

---
+++ data.py
@@ -0,0 +1,21 @@
+file_types = {
+
+'media_formats': ['dpx', 'exr', 'png', 'tiff', 'jpeg', 'hdr', 'rat'], # Media formats collection
+'arhives': ['zip', 'rar', '7zip', 'tar'] }
+
+folders_pattern = {
+
+'project_pattern': ['source', 'result', 'comp', 'render', 'track'],
+'result_pattern' : ['exr', 'png', 'tiff', 'dpx']
+}
+
+def set_project_tree(pattern):
+
+
+def set_result_tree(pattern):
+
+
+
+print(folders_pattern.items())
+
+	
