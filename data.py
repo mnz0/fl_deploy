45055b344c1b34d68bacd1f883afa3ea5aa8967c
mnz0 <mansaaa@gmail.com>
2019-04-20 23:49:56 +0300

    add project_folders_deploy function

--- data.py
+++ data.py
@@ -1,21 +1,29 @@
-file_types = {
+import os
 
-'media_formats': ['dpx', 'exr', 'png', 'tiff', 'jpeg', 'hdr', 'rat'], # Media formats collection
-'arhives': ['zip', 'rar', '7zip', 'tar'] }
+project_dir = ''
+
+file_types = {
+	'media_formats': ['dpx', 'exr', 'png', 'tiff', 'jpeg', 'hdr', 'rat'], # Media formats collection
+	'arhives': ['zip', 'rar', '7zip', 'tar'] }
 
 folders_pattern = {
-
-'project_pattern': ['source', 'result', 'comp', 'render', 'track'],
-'result_pattern' : ['exr', 'png', 'tiff', 'dpx']
-}
-
-def set_project_tree(pattern):
-
-
-def set_result_tree(pattern):
+	'project_pattern': ['source', 'result', 'comp', 'render', 'track'],
+	'result_pattern': ['exr', 'png', 'tiff', 'dpx']}
 
 
 
-print(folders_pattern.items())
 
-	
+def project_folders_deploy():
+
+	""" This function makes folder structure for source files, ref and result sequences.
+		Folders struct get from "folder_patterns" dict.
+	"""
+
+	for folders_name in folders_pattern['project_pattern']:
+			if 'result' in folders_pattern['project_pattern']: 
+				print(folders_name.upper())
+			else:
+				print('Folder "result" is not exist')
+
+
+project_folders_deploy()
